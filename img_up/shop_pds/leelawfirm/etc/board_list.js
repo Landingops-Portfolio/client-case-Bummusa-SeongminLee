6cfb2ad72405358c981ad8a263506def454c58e3
minwookang5687 <softman007@naver.com>
2025-12-03 17:43:38 +0900

    g
    g

---
+++ img_up/shop_pds/leelawfirm/etc/board_list.js
@@ -0,0 +1,3 @@
+var board_list_code = new Array ('board1','board2','board3','board4','board5','notice')
+var board_list_name = new Array ('형사소송사례','민사소송사례','가사소송사례','회생·파산사례','회생파산커뮤니티','공지사항')
+
---
+++ img_up/shop_pds/leelawfirm/etc/navi_category_all.js
@@ -0,0 +1,71 @@
+var navi_cate_00000000_code = new Array ("6db35663","1c2947b2","98051f5d","696e888d","8713652e","a3eab116")
+var navi_cate_00000000_menu_subject = new Array ("법무사 소개","회생·파산","형사소송","민사소송","가사소송","상담 및 소식")
+var navi_cate_00000000_link_url = new Array ("/page/intro","/page/law4","/page/law1","/page/law2","/page/law3","/myreg/oline")
+var navi_cate_00000000_link_target = new Array ("","","","","","")
+var navi_cate_00000000_bt1 = new Array ("","","","","","")
+var navi_cate_00000000_bt2 = new Array ("","","","","","")
+
+var navi_cate_1c2947b2_code = new Array ("2e4cd123","d987d5ac","board5")
+var navi_cate_1c2947b2_menu_subject = new Array ("개요","회생·파산사례","회생파산커뮤니티")
+var navi_cate_1c2947b2_link_url = new Array ("/page/law4","/bbs/board4","/bbs/board5")
+var navi_cate_1c2947b2_link_target = new Array ("","","")
+var navi_cate_1c2947b2_bt1 = new Array ("","","")
+var navi_cate_1c2947b2_bt2 = new Array ("","","")
+
+var navi_cate_696e888d_code = new Array ("1e4d0d26","7da6928d")
+var navi_cate_696e888d_menu_subject = new Array ("개요","민사소송사례")
+var navi_cate_696e888d_link_url = new Array ("/page/law2","/bbs/board2")
+var navi_cate_696e888d_link_target = new Array ("","")
+var navi_cate_696e888d_bt1 = new Array ("","")
+var navi_cate_696e888d_bt2 = new Array ("","")
+
+var navi_cate_6db35663_code = new Array ("1e3d6daf","37798929")
+var navi_cate_6db35663_menu_subject = new Array ("인사말","오시는 길")
+var navi_cate_6db35663_link_url = new Array ("/page/intro","/page/location")
+var navi_cate_6db35663_link_target = new Array ("","")
+var navi_cate_6db35663_bt1 = new Array ("","")
+var navi_cate_6db35663_bt2 = new Array ("","")
+
+var navi_cate_8713652e_code = new Array ("ee48c615","f13d8d25")
+var navi_cate_8713652e_menu_subject = new Array ("개요","가사소송사례")
+var navi_cate_8713652e_link_url = new Array ("/page/law3","/bbs/board3")
+var navi_cate_8713652e_link_target = new Array ("","")
+var navi_cate_8713652e_bt1 = new Array ("","")
+var navi_cate_8713652e_bt2 = new Array ("","")
+
+var navi_cate_98051f5d_code = new Array ("0caed8e4","ffff952e")
+var navi_cate_98051f5d_menu_subject = new Array ("개요","형사소송사례")
+var navi_cate_98051f5d_link_url = new Array ("/page/law1","/bbs/board1")
+var navi_cate_98051f5d_link_target = new Array ("","")
+var navi_cate_98051f5d_bt1 = new Array ("","")
+var navi_cate_98051f5d_bt2 = new Array ("","")
+
+var navi_cate_a3eab116_code = new Array ("bcc548d8","8e9192b3","368deda3")
+var navi_cate_a3eab116_menu_subject = new Array ("온라인상담","방문상담신청","공지사항")
+var navi_cate_a3eab116_link_url = new Array ("/myreg/oline","/myreg/visit","/bbs/notice")
+var navi_cate_a3eab116_link_target = new Array ("","","")
+var navi_cate_a3eab116_bt1 = new Array ("","","")
+var navi_cate_a3eab116_bt2 = new Array ("","","")
+
+
+var navi_path_arr = new Array();
+navi_path_arr['0caed8e4'] = '&gt;&gt;&gt;개요'; 
+navi_path_arr['1c2947b2'] = '&gt;&gt;&gt;회생·파산'; 
+navi_path_arr['1e3d6daf'] = '&gt;&gt;&gt;인사말'; 
+navi_path_arr['1e4d0d26'] = '&gt;&gt;&gt;개요'; 
+navi_path_arr['2e4cd123'] = '&gt;&gt;&gt;개요'; 
+navi_path_arr['368deda3'] = '&gt;&gt;&gt;공지사항'; 
+navi_path_arr['37798929'] = '&gt;&gt;&gt;오시는 길'; 
+navi_path_arr['696e888d'] = '&gt;&gt;&gt;민사소송'; 
+navi_path_arr['6db35663'] = '&gt;&gt;&gt;법무사 소개'; 
+navi_path_arr['7da6928d'] = '&gt;&gt;&gt;민사소송사례'; 
+navi_path_arr['8713652e'] = '&gt;&gt;&gt;가사소송'; 
+navi_path_arr['8e9192b3'] = '&gt;&gt;&gt;방문상담신청'; 
+navi_path_arr['98051f5d'] = '&gt;&gt;&gt;형사소송'; 
+navi_path_arr['a3eab116'] = '&gt;&gt;&gt;상담 및 소식'; 
+navi_path_arr['bcc548d8'] = '&gt;&gt;&gt;온라인상담'; 
+navi_path_arr['board5'] = '&gt;&gt;&gt;회생파산커뮤니티'; 
+navi_path_arr['d987d5ac'] = '&gt;&gt;&gt;회생·파산사례'; 
+navi_path_arr['ee48c615'] = '&gt;&gt;&gt;개요'; 
+navi_path_arr['f13d8d25'] = '&gt;&gt;&gt;가사소송사례'; 
+navi_path_arr['ffff952e'] = '&gt;&gt;&gt;형사소송사례'; 
